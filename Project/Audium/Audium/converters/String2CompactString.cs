fc1d6f8e8f1b13f4d9eca1392d62231aa018c91b
guassailly <guassailly@a3e09f69-07df-4c93-8226-d93fd7a4d72c>
2021-05-12 16:07:47 +0000

    Ajout de deux converters String2CompactString.cs et String2Image.cs Le dico n'est plus static Binding du dico dans un item contol de canvas

---
+++ Project/Audium/Audium/converters/String2CompactString.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Audium.converters
+{
+    class String2CompactString : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string titre = value as string;
+            if (titre.Length > 25)
+            {
+
+                titre = String.Concat(titre.Substring(0,22),"...");
+            }
+            return titre;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
