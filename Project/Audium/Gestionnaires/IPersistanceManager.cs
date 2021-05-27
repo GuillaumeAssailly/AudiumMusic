c5dfb1372badbe6cb8ef2fe42cadb6b15ae142c5
yaboyer <yaboyer@a3e09f69-07df-4c93-8226-d93fd7a4d72c>
2021-05-27 14:41:37 +0000

    Ajout du fichier IPersistanceManager.cs

---
+++ Project/Audium/Gestionnaires/IPersistanceManager.cs
@@ -0,0 +1,16 @@
+﻿using Donnees;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestionnaires
+{
+    public interface IPersistanceManager
+    {
+        (Dictionary<EnsembleAudio, LinkedList<Piste>> mediatheque, List<EnsembleAudio> listeFavoris)ChargeDonnees();
+
+        void SauvegardeDonnees(Dictionary<EnsembleAudio, LinkedList<Piste>> mediatheque, List<EnsembleAudio> listeFavoris);
+    }
+}
